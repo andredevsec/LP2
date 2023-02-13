c551ca8f4a30a88b92c4f9da11b30c0b30bab53c
André Luiz de Lima <84422477+andredevsec@users.noreply.github.com>
2023-02-13 12:20:11 -0300

    Update ex14.java

--- Lista1/ex14.java
+++ Lista1/ex14.java
@@ -9,7 +9,7 @@ public class ex14 {
         System.out.println("entre com o palavra: ");
         String palavra = scan.next();
         for(int i=0; i<palavra.length(); i++){
-            for(int n=1; n<=palavra.length(); n++){
+            for(int n=1; n<palavra.length(); n++){
                 if(palavra.substring(i, n).equals("a") || palavra.substring(i, n).equals("A")){
                     cont++;                
                 }
