4a94c6b3cd629aa71e24b658a19bc51558f4a622
zmm29 <andre.all3@hotmail.com>
2023-02-14 00:46:26 -0300

    ex15 e 16 15

--- Lista1/ex15.java
+++ Lista1/ex15.java
@@ -4,6 +4,21 @@ import java.util.Scanner;
 
 public class ex15 {
     public static void main(String[] args) {
-        
+        Scanner scan = new Scanner(System.in);
+        System.out.print("Digite um número: ");
+        int num = scan.nextInt();
+
+        String numStr = Integer.toString(num);
+        String numStrInvertido = "";
+
+        for (int i = numStr.length() - 1; i >= 0; i--) {
+            numStrInvertido += numStr.substring(i, i + 1);
+        }
+
+        if (numStr.equals(numStrInvertido)) {
+            System.out.println(num + " é uma capicua.");
+        } else {
+            System.out.println(num + " não é uma capicua.");
+        }
     }
 }
